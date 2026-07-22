24dff33d03c7430aeab0aa507cba941894e30681
kam557 <kam557@pitt.edu>
2026-07-22 15:15:22 -0400

    Update Schema class to include summary field
    Replaced existing fields with a new 'summary' field in the Schema class.

--- data/tables/evidence/schema.py
+++ data/tables/evidence/schema.py
@@ -2,10 +2,9 @@ from pydantic import Field
 from adld_track_hub.utils.models import RowData
 from typing import final
 
+from adld_track_hub.utils.models import RowData
+
+
 @final
 class Schema(RowData):
-    publications: int | None = Field(alias = "Publications")
-    literature_cases: int | None = Field(alias = "Literature case labels")
-    families: int | None = Field(alias = "Multigenerational families")
-    estimated_individuals: str | None = Field(alias = "Estimated unique affected individuals")
-    status: str | None = Field(alias = "Deduplication status")
+    summary: str | None = Field(alias = "Summary")
